b63c92fda3fdc1f6e889db63730de9c3354351fa
Lyaaaaaaaaaaaaaaa <lyaaaaaaaaaaaaaaa@protonmail.com>
2022-02-24 16:50:16 +0100

    Added the possibility to choose to support cuda or not.

--- install.sh
+++ install.sh
@@ -17,12 +17,15 @@
 #-- Changelog:
 #--   21/02/2022 Lyaaaaa
 #--     - Created the script.
+#--
+#--   24/02/2022 Lyaaaaa
+#--     - Updated the script to give the option to install cuda or not.
 #---------------------------------------------------------------------------
 
 mini_conda_link="https://repo.anaconda.com/miniconda/Miniconda3-py39_4.10.3-Linux-x86_64.sh"
 cache_folder="cache"
 environment_prefix="server/miniconda/"
-conda_environment_file="conda_config.yml"
+conda_environment_file="conda_config"
 
 function download_conda()
 {
@@ -38,10 +41,40 @@ function install_conda()
 
 function create_environment()
 {
+  if [[ $1 = true ]]
+  then
+    conda_environment_file=$conda_environment_file"_cuda.yml"
+
+  elif [[ $1 = false ]]
+  then
+    conda_environment_file=$conda_environment_file".yml"
+  fi
+
   conda_bin_path=$environment_prefix"bin/conda"
   ./$conda_bin_path env create --file $conda_environment_file
 }
 
-download_conda
-install_conda
-create_environment
+# download_conda
+# install_conda
+
+# Not in a function because clear doesn't work in functions. No clue why.
+loop=true
+cuda=false
+
+while $loop = true
+do
+  read -n 1 -p "Do you want to install CUDA's support? It will download more and increase the environment's size. [Y/N]" input
+  clear
+  if [[ $input = 'y' ]] || [[ $input = 'Y' ]]
+  then
+    loop=false
+    cuda=true
+
+  elif [[ $input = 'n' ]] || [[ $input = 'N' ]]
+  then
+    loop=false
+    cuda=false
+  fi
+done
+
+create_environment "$cuda"
